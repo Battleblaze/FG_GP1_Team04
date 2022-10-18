d6fba0394337e8d2dc42be7f237badc2003f025e
Kycklingris <malte.linde.neveling@gmail.com>
2022-10-18 14:41:46 +0200

    material changes

--- GP1_Team04_Project/Assets/Scripts/Collectibles/RotateCollectible.cs
+++ GP1_Team04_Project/Assets/Scripts/Collectibles/RotateCollectible.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 
 namespace FG.Collectibles
 {
-    public class RotateCollectible : MonoBehaviour
-    {
-        [SerializeField]
-        private float _rotateSpeed;
+	public class RotateCollectible : MonoBehaviour
+	{
+		[SerializeField]
+		private float _rotateSpeed;
 
-        private Renderer _renderer;
+		private Renderer _renderer;
 
-        private void Start()
-        {
-            _renderer = GetComponentInChildren<Renderer>();
-        }
+		private void Start()
+		{
+			transform.Rotate(new Vector3(0.0f, Random.Range(0f, 360f), 0.0f));
 
-        void Update()
-        {
-            if (_renderer.isVisible) transform.Rotate(0, _rotateSpeed * Time.deltaTime, 0, Space.Self);
-        }
-    }
+			this._rotateSpeed *= Random.Range(0.75f, 1.5f);
+
+			_renderer = GetComponentInChildren<Renderer>();
+		}
+
+		void Update()
+		{
+			if (_renderer.isVisible) transform.Rotate(0, _rotateSpeed * Time.deltaTime, 0, Space.Self);
+		}
+	}
 }
