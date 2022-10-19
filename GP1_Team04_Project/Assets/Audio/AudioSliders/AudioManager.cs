f6f3bacfae58577aa0fc9c9746f44daf78d48375
Mark9901 <78760951+Mark9901@users.noreply.github.com>
2022-10-20 00:15:57 +0200

    clean up audio manager code
    need to fix: ui button sounds not working when switching scene, pause audio on pause.

--- GP1_Team04_Project/Assets/Audio/AudioSliders/AudioManager.cs
+++ GP1_Team04_Project/Assets/Audio/AudioSliders/AudioManager.cs
@@ -1,25 +1,24 @@
 
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using FG.Managers;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
     [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioSource SFXSource;
+    [SerializeField] AudioSource alwaysOnSource;
 
-    [SerializeField] AudioSource buttonPressSource;
     [SerializeField] private AudioClip buttonPressClip;
-
-    [SerializeField] AudioSource HPCollectSource;
     [SerializeField] private AudioClip HPCollectClip;
-
-    [SerializeField] AudioSource hurtSoundSource;
     [SerializeField] private AudioClip hurtSoundClip;
-
-    [SerializeField] AudioSource coinCollectSource;
     [SerializeField] private AudioClip coinCollectClip;
+    [SerializeField] private AudioClip backgroundClip;
+    [SerializeField] private AudioClip hoverboardClip;
 
     public const string MUSIC_KEY = "MusicVolume";
     public const string SFX_KEY = "SFXVolume";
@@ -37,7 +36,6 @@ public class AudioManager : MonoBehaviour
         }
         LoadVolume();
     }
-
     void LoadVolume() //Volume saved in VolumeSettings
     {
         float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY, 1f);
@@ -51,24 +49,39 @@ public class AudioManager : MonoBehaviour
     public void CoinCollectSFX()
     {
         AudioClip coinclip = coinCollectClip;
-        coinCollectSource.PlayOneShot(coinclip);
+        SFXSource.PlayOneShot(coinclip);
     }
 
     public void HPCollectSFX()
     {
         AudioClip HPclip = HPCollectClip;
-        HPCollectSource.PlayOneShot(HPclip);
+        SFXSource.PlayOneShot(HPclip);
     }
 
     public void HurtSoundSFX()
     {
         AudioClip Hurtclip = hurtSoundClip;
-        hurtSoundSource.PlayOneShot(Hurtclip);
+        SFXSource.PlayOneShot(Hurtclip);
     }
     public void ButtonPress()
     {
         AudioClip buttonClip = buttonPressClip;
-        buttonPressSource.PlayOneShot(buttonClip);
+        SFXSource.PlayOneShot(buttonClip);
+        Debug.Log("ButtonPressed");
     }
 
+    //public void PauseAudio()
+    //{
+
+    //    if (GameObject.Find("GameManager").GetComponent<GameManager>().pausedgame == true)
+    //    {
+    //        alwaysOnSource.Pause();
+            
+    //    }
+    //    else
+    //    {
+    //        alwaysOnSource.Play();
+    //        Debug.Log("NotPaused");  
+    //    }
+    //}
 }
