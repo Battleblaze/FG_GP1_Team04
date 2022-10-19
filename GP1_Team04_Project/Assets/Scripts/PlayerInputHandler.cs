dd06cb48c8be50f0936f8d3057e9bc09cf28e3c4
Kycklingris <malte.linde.neveling@gmail.com>
2022-10-19 10:56:01 +0200

    pot fixed

---
+++ GP1_Team04_Project/Assets/Scripts/PlayerInputHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using FG.Managers;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+	[SerializeField] private GameObject pauseMenu;
+
+	public void A(InputAction.CallbackContext context)
+    {
+		if(context.performed)
+        {
+		    this.pauseMenu.SetActive(true);
+        }
+	}
+
+    public void B(InputAction.CallbackContext context)
+    {
+
+    }
+}
